f52813a8ae4bcd0c489a4d7cff057ea839489985
Evandro Pomatti <evandro.pomatti@gmail.com>
2023-06-11 11:36:37 -0300

    message loeading

--- dataload/DataLoader.cs
+++ dataload/DataLoader.cs
@@ -96,92 +96,116 @@ class DataLoader
     dataTable.Columns.Add(Priority);
     dataTable.Columns.Add(CreatedAt);
 
-    // DataColumn Text01 = new DataColumn("text01");
-    // DataColumn Text02 = new DataColumn("text02");
-    // DataColumn Text03 = new DataColumn("text03");
-    // DataColumn Text04 = new DataColumn("text04");
-    // DataColumn Text05 = new DataColumn("text05");
-    // DataColumn Text06 = new DataColumn("text06");
-    // DataColumn Text07 = new DataColumn("text07");
-    // DataColumn Text08 = new DataColumn("text08");
-    // DataColumn Text09 = new DataColumn("text09");
-    // DataColumn Text10 = new DataColumn("text10");
-    // DataColumn Text11 = new DataColumn("text11");
-    // DataColumn Text12 = new DataColumn("text12");
-    // DataColumn Text13 = new DataColumn("text13");
-    // DataColumn Text14 = new DataColumn("text14");
-    // DataColumn Text15 = new DataColumn("text15");
-    // DataColumn Text16 = new DataColumn("text16");
-    // DataColumn Text17 = new DataColumn("text17");
-    // DataColumn Text18 = new DataColumn("text18");
-    // DataColumn Text19 = new DataColumn("text19");
-    // DataColumn Text20 = new DataColumn("text20");
+    DataColumn Text01 = new DataColumn("Text01");
+    DataColumn Text02 = new DataColumn("Text02");
+    DataColumn Text03 = new DataColumn("Text03");
+    DataColumn Text04 = new DataColumn("Text04");
+    DataColumn Text05 = new DataColumn("Text05");
+    DataColumn Text06 = new DataColumn("Text06");
+    DataColumn Text07 = new DataColumn("Text07");
+    DataColumn Text08 = new DataColumn("Text08");
+    DataColumn Text09 = new DataColumn("Text09");
+    DataColumn Text10 = new DataColumn("Text10");
+    DataColumn Text11 = new DataColumn("Text11");
+    DataColumn Text12 = new DataColumn("Text12");
+    DataColumn Text13 = new DataColumn("Text13");
+    DataColumn Text14 = new DataColumn("Text14");
+    DataColumn Text15 = new DataColumn("Text15");
+    DataColumn Text16 = new DataColumn("Text16");
+    DataColumn Text17 = new DataColumn("Text17");
+    DataColumn Text18 = new DataColumn("Text18");
+    DataColumn Text19 = new DataColumn("Text19");
+    DataColumn Text20 = new DataColumn("Text20");
 
     // DataColumn FKOrganization = new DataColumn("fk_organization");
 
-    // dataTable.Columns.Add(Text01);
-    // dataTable.Columns.Add(Text02);
-    // dataTable.Columns.Add(Text03);
-    // dataTable.Columns.Add(Text04);
-    // dataTable.Columns.Add(Text05);
-    // dataTable.Columns.Add(Text06);
-    // dataTable.Columns.Add(Text07);
-    // dataTable.Columns.Add(Text08);
-    // dataTable.Columns.Add(Text09);
-    // dataTable.Columns.Add(Text10);
-    // dataTable.Columns.Add(Text11);
-    // dataTable.Columns.Add(Text12);
-    // dataTable.Columns.Add(Text13);
-    // dataTable.Columns.Add(Text14);
-    // dataTable.Columns.Add(Text15);
-    // dataTable.Columns.Add(Text16);
-    // dataTable.Columns.Add(Text17);
-    // dataTable.Columns.Add(Text18);
-    // dataTable.Columns.Add(Text19);
-    // dataTable.Columns.Add(Text20);
+    dataTable.Columns.Add(Text01);
+    dataTable.Columns.Add(Text02);
+    dataTable.Columns.Add(Text03);
+    dataTable.Columns.Add(Text04);
+    dataTable.Columns.Add(Text05);
+    dataTable.Columns.Add(Text06);
+    dataTable.Columns.Add(Text07);
+    dataTable.Columns.Add(Text08);
+    dataTable.Columns.Add(Text09);
+    dataTable.Columns.Add(Text10);
+    dataTable.Columns.Add(Text11);
+    dataTable.Columns.Add(Text12);
+    dataTable.Columns.Add(Text13);
+    dataTable.Columns.Add(Text14);
+    dataTable.Columns.Add(Text15);
+    dataTable.Columns.Add(Text16);
+    dataTable.Columns.Add(Text17);
+    dataTable.Columns.Add(Text18);
+    dataTable.Columns.Add(Text19);
+    dataTable.Columns.Add(Text20);
 
     for (int i = 0; i < msgsQty; i++)
     {
       object?[] values = {
         1,
-        DateTime.Now
+        DateTime.Now,
+        "Text01",
+        "Text02",
+        "Text03",
+        "Text04",
+        "Text05",
+        "Text06",
+        "Text07",
+        "Text08",
+        "Text09",
+        "Text10",
+        "Text11",
+        "Text12",
+        "Text13",
+        "Text14",
+        "Text15",
+        "Text16",
+        "Text17",
+        "Text18",
+        "Text19",
+        "Text20",
       };
       dataTable.Rows.Add(values);
-      // dataTable.Rows.Add({1, DateTime.Now}); // Priority
-      // dataTable.Rows.Add(DateTime.Now); // Created At
-
-
-      // dataTable.Rows.Add("text02");
-      // dataTable.Rows.Add("text01");
-      // dataTable.Rows.Add("text03");
-      // dataTable.Rows.Add("text04");
-      // dataTable.Rows.Add("text05");
-      // dataTable.Rows.Add("text06");
-      // dataTable.Rows.Add("text07");
-      // dataTable.Rows.Add("text08");
-      // dataTable.Rows.Add("text09");
-      // dataTable.Rows.Add("text10");
-      // dataTable.Rows.Add("text11");
-      // dataTable.Rows.Add("text12");
-      // dataTable.Rows.Add("text13");
-      // dataTable.Rows.Add("text14");
-      // dataTable.Rows.Add("text15");
-      // dataTable.Rows.Add("text16");
-      // dataTable.Rows.Add("text17");
-      // dataTable.Rows.Add("text18");
-      // dataTable.Rows.Add("text19");
-      // dataTable.Rows.Add("text20");
 
       // dataTable.Rows.Add(orgFK);
     }
 
     SqlDataAdapter adapter = new SqlDataAdapter();
 
-    adapter.InsertCommand = new SqlCommand("INSERT INTO Message (Priority, CreatedAt) VALUES (@Priority, @CreatedAt);", connection);
+    var cmd = @"
+    INSERT INTO Message
+    (Priority, CreatedAt, Text01, Text02, Text03, Text04, Text05, Text06, Text07, Text08, Text09, Text10, Text11, Text12, Text13, Text14, Text15, Text16, Text17, Text18, Text19, Text20)
+    VALUES
+    (@Priority, @CreatedAt, @Text01, @Text02, @Text03, @Text04, @Text05, @Text06, @Text07, @Text08, @Text09, @Text10, @Text11, @Text12, @Text13, @Text14, @Text15, @Text16, @Text17, @Text18, @Text19, @Text20);
+    ";
+
+    adapter.InsertCommand = new SqlCommand(cmd, connection);
+
     adapter.InsertCommand.Parameters.Add("@Priority", SqlDbType.Int, 0, "Priority");
     adapter.InsertCommand.Parameters.Add("@CreatedAt", SqlDbType.DateTime2, 0, "CreatedAt");
-    adapter.InsertCommand.UpdatedRowSource = UpdateRowSource.None;
+    adapter.InsertCommand.Parameters.Add("@Text01", SqlDbType.NVarChar, 50, "Text01");
+    adapter.InsertCommand.Parameters.Add("@Text02", SqlDbType.NVarChar, 50, "Text02");
+    adapter.InsertCommand.Parameters.Add("@Text03", SqlDbType.NVarChar, 50, "Text03");
+    adapter.InsertCommand.Parameters.Add("@Text04", SqlDbType.NVarChar, 50, "Text04");
+    adapter.InsertCommand.Parameters.Add("@Text05", SqlDbType.NVarChar, 50, "Text05");
+    adapter.InsertCommand.Parameters.Add("@Text06", SqlDbType.NVarChar, 50, "Text06");
+    adapter.InsertCommand.Parameters.Add("@Text07", SqlDbType.NVarChar, 50, "Text07");
+    adapter.InsertCommand.Parameters.Add("@Text08", SqlDbType.NVarChar, 50, "Text08");
+    adapter.InsertCommand.Parameters.Add("@Text09", SqlDbType.NVarChar, 50, "Text09");
+    adapter.InsertCommand.Parameters.Add("@Text10", SqlDbType.NVarChar, 50, "Text10");
+    adapter.InsertCommand.Parameters.Add("@Text11", SqlDbType.NVarChar, 50, "Text11");
+    adapter.InsertCommand.Parameters.Add("@Text12", SqlDbType.NVarChar, 50, "Text12");
+    adapter.InsertCommand.Parameters.Add("@Text13", SqlDbType.NVarChar, 50, "Text13");
+    adapter.InsertCommand.Parameters.Add("@Text14", SqlDbType.NVarChar, 50, "Text14");
+    adapter.InsertCommand.Parameters.Add("@Text15", SqlDbType.NVarChar, 50, "Text15");
+    adapter.InsertCommand.Parameters.Add("@Text16", SqlDbType.NVarChar, 50, "Text16");
+    adapter.InsertCommand.Parameters.Add("@Text17", SqlDbType.NVarChar, 50, "Text17");
+    adapter.InsertCommand.Parameters.Add("@Text18", SqlDbType.NVarChar, 50, "Text18");
+    adapter.InsertCommand.Parameters.Add("@Text19", SqlDbType.NVarChar, 50, "Text19");
+    adapter.InsertCommand.Parameters.Add("@Text20", SqlDbType.NVarChar, 50, "Text20");
 
+    adapter.InsertCommand.UpdatedRowSource = UpdateRowSource.None;
     adapter.UpdateBatchSize = batchSize;
 
     int updated = adapter.Update(dataTable);
--- schema.sql
+++ schema.sql
@@ -15,5 +15,41 @@ ALTER TABLE [dbo].[Organization] ADD  CONSTRAINT [PK_NewTable] PRIMARY KEY CLUST
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 GO
 
-
 -- Messages
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Message](
+	[rowid] [int] IDENTITY(1,1) NOT NULL,
+	[Priority] [int] NULL,
+	[CreatedAt] [datetime2](7) NULL,
+	[Text01] [nvarchar](50) NULL,
+	[Text02] [nvarchar](50) NULL,
+	[Text03] [nvarchar](50) NULL,
+	[Text04] [nvarchar](50) NULL,
+	[Text05] [nvarchar](50) NULL,
+	[Text06] [nvarchar](50) NULL,
+	[Text07] [nvarchar](50) NULL,
+	[Text08] [nvarchar](50) NULL,
+	[Text09] [nvarchar](50) NULL,
+	[Text10] [nvarchar](50) NULL,
+	[Text11] [nvarchar](50) NULL,
+	[Text12] [nvarchar](50) NULL,
+	[Text13] [nvarchar](50) NULL,
+	[Text14] [nvarchar](50) NULL,
+	[Text15] [nvarchar](50) NULL,
+	[Text16] [nvarchar](50) NULL,
+	[Text17] [nvarchar](50) NULL,
+	[Text18] [nvarchar](50) NULL,
+	[Text19] [nvarchar](50) NULL,
+	[Text20] [nvarchar](50) NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Message] ADD  CONSTRAINT [PK_Message] PRIMARY KEY CLUSTERED 
+(
+	[rowid] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
+
