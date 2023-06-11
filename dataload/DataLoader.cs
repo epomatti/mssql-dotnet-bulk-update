ef504c88775b83093b9a904b981797eb83cd607a
Evandro Pomatti <evandro.pomatti@gmail.com>
2023-06-11 10:09:38 -0300

    camel fix

--- dataload/DataLoader.cs
+++ dataload/DataLoader.cs
@@ -46,14 +46,14 @@ class DataLoader
 
     Console.WriteLine("\nCreating Organizations...");
     int batchSize = Int32.Parse(Environment.GetEnvironmentVariable("ORGANIZATIONS_BATCH_SIZE")!);
-    int orgsQuty = Int32.Parse(Environment.GetEnvironmentVariable("ORGANIZATIONS_QUANTITY")!);
+    int orgsQty = Int32.Parse(Environment.GetEnvironmentVariable("ORGANIZATIONS_QUANTITY")!);
     string prefix = "My Organization ";
 
     DataTable dataTable = new DataTable("Organizations");
-    DataColumn Name = new DataColumn("name");
+    DataColumn Name = new DataColumn("Name");
     dataTable.Columns.Add(Name);
 
-    for (int i = 0; i < orgsQuty; i++)
+    for (int i = 0; i < orgsQty; i++)
     {
       string n = i.ToString().PadLeft(3, '0');
       dataTable.Rows.Add(prefix + n);
@@ -61,8 +61,8 @@ class DataLoader
 
     SqlDataAdapter adapter = new SqlDataAdapter();
 
-    adapter.InsertCommand = new SqlCommand("INSERT INTO Organizations (name) VALUES (@Name);", connection);
-    adapter.InsertCommand.Parameters.Add("@Name", SqlDbType.NVarChar, 50, "name");
+    adapter.InsertCommand = new SqlCommand("INSERT INTO Organizations (Name) VALUES (@Name);", connection);
+    adapter.InsertCommand.Parameters.Add("@Name", SqlDbType.NVarChar, 50, "Name");
     adapter.InsertCommand.UpdatedRowSource = UpdateRowSource.None;
 
     adapter.UpdateBatchSize = batchSize;
